06e13fb622d8745acbcbd48af4496c67cdd2bc2b
Fabian Mendoza <yessid.mendoza@gmail.com>
2018-02-23 03:07:39 -0500

    menu update

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,19 +1,19 @@
 import {Routes, RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
 
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {NewsComponent} from './news/news.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {NewsComponent} from './components/news/news.component';
 
-const routesForEvent: Routes = [
+const routes: Routes = [
     {path: 'dashboard', component:DashboardComponent},
     {path: 'news', component: NewsComponent},
     {path: '', component: DashboardComponent},
-    {path: '**', component: DashboardComponent}
+    {path: '**', pathMatch:'full', redirectTo: 'dashboard'}
   ]; 
 
   @NgModule({
       imports:[
-        RouterModule.forRoot(routesForEvent)
+        RouterModule.forRoot(routes)
       ], 
       exports:[
           RouterModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NewsComponent } from './news/news.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NewsComponent } from './components/news/news.component';
 import { AppRoutingModule } from './app-routing-module';
-import { HomeComponent } from './home/home.component';
-import { SpeedZonesComponent } from './speed-zones/speed-zones.component';
-import { CountByZonesComponent } from './count-by-zones/count-by-zones.component';
-import { SpeedAverageComponent } from './speed-average/speed-average.component';
+import { HomeComponent } from './components/home/home.component';
+import { SpeedZonesComponent } from './components/speed-zones/speed-zones.component';
+import { CountByZonesComponent } from './components/count-by-zones/count-by-zones.component';
+import { SpeedAverageComponent } from './components/speed-average/speed-average.component';
 
 @NgModule({
   declarations: [
